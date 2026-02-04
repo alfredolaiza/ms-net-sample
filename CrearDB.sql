69683382ad35a27f78e5e09e4324198cb1b191a9
Alfredo Benaute <alfredo.benaute@gmail.com>
2026-02-03 21:29:43 -0500

    fix: scripts de tablas

--- CrearDB.sql
+++ CrearDB.sql
@@ -32,3 +32,37 @@ CREATE TABLE [dbo].[TodoLists](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+
+
+
+CREATE TABLE [dbo].[Categories](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+	[Description] [varchar](max) NULL,
+	[Created] [datetimeoffset](7) NULL,
+	[CreatedBy] [varchar](max) NULL,
+	[LastModified] [datetimeoffset](7) NULL,
+	[LastModifiedBy] [varchar](max) NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
+GO
+
+/* Añadir tabla producto */
+CREATE TABLE [dbo].[Products](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NOT NULL,
+	[Description] [varchar](max) NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[Stock] [int] NOT NULL,
+	[Created] [datetimeoffset](7) NULL,
+	[CreatedBy] [varchar](max) NULL,
+	[LastModified] [datetimeoffset](7) NULL,
+	[LastModifiedBy] [varchar](max) NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
